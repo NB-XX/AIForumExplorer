146f28b7069a44ac8db9d25c818e0b1c3e3a4308
NB-XX <53631369+NB-XX@users.noreply.github.com>
2025-07-08 21:16:17 +0800

    Update streamlit_app.py
    更新可用模型列表

--- streamlit_app.py
+++ streamlit_app.py
@@ -133,11 +133,9 @@ with col2:
 
 # 模型选择
 model_options = {
-    "gemini-2.0-flash": "Gemini 2.0 Flash",
-    "gemini-2.0-pro-exp-02-05": "Gemini 2.0 pro",
-    "gemini-1.5-flash": "Gemini 1.0 Flash (每分钟15次查询，每天1500次查询)",
-    "gemini-1.5-pro-latest": "[因TPM限制无法处理超长文本]Gemini 1.5 Pro (每分钟2次查询，每天50次查询)",
-    "gemini-1.0-pro-latest": "[因TPM限制无法处理超长文本]Gemini 1.0 Pro (每分钟15次查询，每天1500次查询)"
+    "gemini-2.5-flash": "Gemini 2.5 Flash",
+    "gemini-2.5-pro": "Gemini 2.5 Pro",
+    "gemini-2.5-flash-lite-preview-06-17":"Gemini 2.5 Flash-Lite 预览版"
 }
 model_choice = st.selectbox(
     "请选择模型：",
